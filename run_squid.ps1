48feb796970b63fe11f3572bb37f6fd3757e5235
Latour <Louis.Latour@philips.com>
2020-02-12 22:42:38 +0100

    Adding squid

---
+++ run_squid.ps1
@@ -0,0 +1,13 @@
+# see https://hub.docker.com/r/sameersbn/squid/
+
+$loc = Get-Location
+$proxyConfPath = (Join-Path -Path $loc.Path -ChildPath "proxy_squid.conf").ToString()
+
+# write-host Starting nginx with configuration $proxyConfPath
+
+docker run --name squid -d --restart=always `
+  --publish 8888:3128 `
+  --volume ${proxyConfPath}:/etc/squid/squid.conf `
+  sameersbn/squid:3.5.27-2
+
+docker exec -it squid tail -f /var/log/squid/access.log
